6bc036d2517d6ca98e4b7982e90ac2c3ff432162
Jordan Jacobson <jordanjacobson2@gmail.com>
2017-06-27 10:27:09 +1000

    My Game

--- Kenny/ViewController.swift
+++ Kenny/ViewController.swift
@@ -10,16 +10,172 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var kenny1: UIImageView!
+    @IBOutlet weak var kenny2: UIImageView!
+    @IBOutlet weak var kenny3: UIImageView!
+    @IBOutlet weak var kenny4: UIImageView!
+    @IBOutlet weak var kenny5: UIImageView!
+    @IBOutlet weak var kenny6: UIImageView!
+    @IBOutlet weak var kenny7: UIImageView!
+    @IBOutlet weak var kenny8: UIImageView!
+    @IBOutlet weak var kenny9: UIImageView!
+    
+    @IBOutlet weak var highscoreLabel: UILabel!
+    
+    var timer = Timer()
+    var hideTimer = Timer()
+    var score = 0
+    var counter = 30
+    
+    var lastAvailable = -1
+    
+    var kennyArray = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // check highscore
+        
+        let highscore = UserDefaults.standard.object(forKey: "highscore")
+        
+        if highscore == nil {
+            highscoreLabel.text = "0"
+        }
+        
+        if let newScore = highscore as? Int {
+            highscoreLabel.text = String(newScore)
+        }
+        
+            
+        
+        
+        
+        scoreLabel.text = "Score: \(score)"
+        timerLabel.text = "\(counter)"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.countDown), userInfo: nil, repeats: true)
+        hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.hideKenny), userInfo: nil, repeats: true)
+        
+        let recognizer1 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        let recognizer2 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        let recognizer3 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        let recognizer4 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        let recognizer5 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        let recognizer6 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        let recognizer7 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        let recognizer8 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        let recognizer9 = UITapGestureRecognizer(target: self, action: #selector(ViewController.increaseScore))
+        
+        kenny1.addGestureRecognizer(recognizer1)
+        kenny2.addGestureRecognizer(recognizer2)
+        kenny3.addGestureRecognizer(recognizer3)
+        kenny4.addGestureRecognizer(recognizer4)
+        kenny5.addGestureRecognizer(recognizer5)
+        kenny6.addGestureRecognizer(recognizer6)
+        kenny7.addGestureRecognizer(recognizer7)
+        kenny8.addGestureRecognizer(recognizer8)
+        kenny9.addGestureRecognizer(recognizer9)
+        
+        kenny1.isUserInteractionEnabled = true
+        kenny2.isUserInteractionEnabled = true
+        kenny3.isUserInteractionEnabled = true
+        kenny4.isUserInteractionEnabled = true
+        kenny5.isUserInteractionEnabled = true
+        kenny6.isUserInteractionEnabled = true
+        kenny7.isUserInteractionEnabled = true
+        kenny8.isUserInteractionEnabled = true
+        kenny9.isUserInteractionEnabled = true
+    
+        kennyArray.append(kenny1)
+        kennyArray.append(kenny2)
+        kennyArray.append(kenny3)
+        kennyArray.append(kenny4)
+        kennyArray.append(kenny5)
+        kennyArray.append(kenny6)
+        kennyArray.append(kenny7)
+        kennyArray.append(kenny8)
+        kennyArray.append(kenny9)
+        
+        hideKenny()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+
+    func countDown() {
+        counter = counter - 1
+        timerLabel.text = "\(counter)"
+        
+        if (counter == 0) {
+            timer.invalidate()
+            hideTimer.invalidate()
+
+            
+            if self.score > Int(highscoreLabel.text!)! {
+                UserDefaults.standard.set(self.score, forKey: "highscore")
+                highscoreLabel.text = String(self.score)
+            }
+            
+            
+            let alert = UIAlertController(title: "Time", message: "You are out of time", preferredStyle: UIAlertControllerStyle.alert)
+            let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+            let replay = UIAlertAction(title: "Replay", style: UIAlertActionStyle.default, handler: { (UIAlertAction) in
+                
+                self.score = 0
+                self.scoreLabel.text = "Score: \(self.score)"
+                self.counter = 30
+                self.timerLabel.text = "\(self.counter)"
+
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.countDown), userInfo: nil, repeats: true)
+                self.hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(ViewController.hideKenny), userInfo: nil, repeats: true)
+            })
+            
+            alert.addAction(ok)
+            alert.addAction(replay)
+            self.present(alert, animated: true, completion: nil)
+            
+        }
     }
+    
+    func increaseScore() {
+        score = score + 1
+        scoreLabel.text = "Score: \(score)"
 
-
+    }
+    
+    func hideKenny() {
+        
+        for kenny in kennyArray {
+            kenny.isHidden = true
+        }
+        
+        var randomNumber = Int(arc4random_uniform(UInt32(kennyArray.count - 1)))
+        while (randomNumber == lastAvailable) {
+            randomNumber = Int(arc4random_uniform(UInt32(kennyArray.count - 1)))
+        }
+        lastAvailable = randomNumber
+        
+        kennyArray[randomNumber].isHidden = false
+        
+        
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
